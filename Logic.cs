7ac1b5074826bba44895e0fce57fad2c50e11bcd
git config --global hub.protocol https <>
2020-02-10 17:28:21 -0500

    external logic file added not linked

---
+++ Logic.cs
@@ -0,0 +1,11 @@
+
+// {
+// if (user == "Alice")
+//       {
+//     Console.WriteLine("Hello Alice, we've been waiting for you");
+//   }
+//       else
+//       {
+//     Console.WriteLine("Hello " + user);
+//   }
+// }
