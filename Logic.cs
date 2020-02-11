f3d3093c9fd1e4daf913d36e6659ee4b56506ad5
git config --global hub.protocol https <>
2020-02-11 09:28:24 -0500

    Changed logic file to a class file

--- Logic.cs
+++ Logic.cs
@@ -1,11 +1,18 @@
+namespace Variables
+{
+  public class Logic
+  {
 
-// {
-// if (user == "Alice")
-//       {
-//     Console.WriteLine("Hello Alice, we've been waiting for you");
-//   }
-//       else
-//       {
-//     Console.WriteLine("Hello " + user);
-//   }
-// }
+    // {
+    // if (user == "Alice")
+    //       {
+    //     Console.WriteLine("Hello Alice, we've been waiting for you");
+    //   }
+    //       else
+    //       {
+    //     Console.WriteLine("Hello " + user);
+    //   }
+    // }
+
+  }
+}
